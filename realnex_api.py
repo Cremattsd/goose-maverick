7554ad9687c022c9d8a622d5651cbda58e3eb35c
Cremattsd <110553037+Cremattsd@users.noreply.github.com>
2025-04-23 07:51:00 -0700

    Delete realnex_api.py

--- realnex_api.py
+++
@@ -1,9 +0,0 @@
-from real_nex_sync_api_data_facade.sdk import RealNexSyncApiDataFacade
-
-def upload_data_to_realnex(data, user_token):
-    try:
-        client = RealNexSyncApiDataFacade(api_key=user_token)  # User enters their token
-        response = client.upload_data(data)
-        return response
-    except Exception as e:
-        return {"error": str(e)}
