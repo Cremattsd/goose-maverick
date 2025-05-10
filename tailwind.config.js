5aec5f59bb7dc64ff11e8c49dadd747f01560ec7
Cremattsd <110553037+Cremattsd@users.noreply.github.com>
2025-05-10 14:05:45 -0700

    Update tailwind.config.js
    Signed-off-by: Cremattsd <110553037+Cremattsd@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./static/**/*.html", "./app.py"],  // ← updated path!
+  content: ["./static/**/*.html"],  // Remove ./app.py since it’s not needed
   theme: {
     extend: {},
   },
