d0e60f877d9b2b0dfb60dd74aab94a6d3c8b8df7
Visions138 <110553037+Cremattsd@users.noreply.github.com>
2025-05-13 19:28:22 -0700

    Create voice.js
    Signed-off-by: Visions138 <110553037+Cremattsd@users.noreply.github.com>

---
+++ static/js/voice.js
@@ -0,0 +1,15 @@
+// static/js/voice.js
+window.onload = function () {
+  const recognition = new webkitSpeechRecognition();
+  recognition.continuous = false;
+  recognition.interimResults = false;
+  recognition.lang = 'en-US';
+
+  document.getElementById('start-voice').onclick = function () {
+    recognition.start();
+  };
+
+  recognition.onresult = function (e) {
+    document.getElementById('message-input').value = e.results[0][0].transcript;
+  };
+};
