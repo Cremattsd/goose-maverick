5018c93ea29f61fcb5d201ed3d18ed8a72d7d0a3
Visions138 <110553037+Cremattsd@users.noreply.github.com>
2025-05-17 21:03:03 -0700

    Update tailwind.config.js
    Signed-off-by: Visions138 <110553037+Cremattsd@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./static/**/*.html"],
+  content: [
+    "./templates/**/*.html",     // If using Flask templates
+    "./static/**/*.html",        // Static HTML files
+    "./static/**/*.js",          // JS that may use Tailwind classes
+    "./app.py",                  // Tailwind in Flask responses
+    "./static/css/input.css"     // Include the CSS file explicitly
+  ],
   theme: {
     extend: {},
   },
